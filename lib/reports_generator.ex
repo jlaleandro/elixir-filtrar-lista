2855b5c9acc45a031d394cd948f0af0bd95f6188
jlaleandro <jlaleandro@gmail.com>
2021-03-10 17:24:43 -0300

    modulo 2

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,17 +1,44 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
+  @options ["users", "foods"]
+
   def build(filename) do
-    "reports/#{filename}"
-    |> File.read()
-    |> handle_file()
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while opening file!"
-end
+  # teste no iex
+  # "report_complete.csv" |> ReportsGenerator.build() |> ReportsGenerator.fetch_higher_cost("users")
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
 
-# def build(filename) do
-#   case File.read("reports/#{filename}") do
-#     {:ok, result} -> result
-#     {:error, result} -> result
-#   end
-# end
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option!"}
+
+  defp sum_values([id, food_name, price], %{"users" => users, "foods" => foods} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp report_acc do
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+
+    %{"users" => users, "foods" => foods}
+  end
+end
