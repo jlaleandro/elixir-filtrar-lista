1fef14632472af1a4565d25a5cfe78869d337a4b
jlaleandro <jlaleandro@gmail.com>
2021-03-12 10:32:46 -0300

    qtdimparok

--- lib/desafio.ex
+++ lib/desafio.ex
@@ -1,13 +1,11 @@
 defmodule Desafio do
   # lista =  ["1", "3", "6", "43", "banana", "6", "abc"]
-
+  # retorna lista com restos div/2 [1,1,0,1,0]
   def list_filter(lista) do
     result =
       Enum.flat_map(lista, fn string ->
         case Integer.parse(string) do
-          # transform to integer
           {int, _rest} -> [rem(int, 2)]
-          # skip the value
           :error -> []
         end
       end)
@@ -18,16 +16,13 @@ defmodule Desafio do
   defp qtdimpar([], impar), do: impar
 
   defp qtdimpar([head | tail], impar) do
-    IO.inspect(head)
-
-    if head != 0 do
-      impar + 1
-
-      # the result of the expression is ignored (suppress the warning by assigning the expression to the _ variable)
-    else
-      impar
-    end
-
+    impar = if head != 0, do: impar + 1, else: impar
     qtdimpar(tail, impar)
   end
+
+  # ou
+  # impar = evaluate_head(head, impar)
+
+  # defp evaluate_head(0, impar), do: impar
+  # defp evaluate_head(_, impar), do: impar + 1
 end
